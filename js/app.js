ecd559b30fb827c3c609cfc7307ff959a1398470
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2024-10-13 14:21:24 +0200

    añadiendo ejercicio 6 formulario

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*
+Desarrollar un formulario de contacto con un evento que cuando se quite el focus del input,
+verifique que lo que se ha metido en el input es correcto (el
+asunto no está vacío, cc es un correo, el cuerpo del mensaje
+no está vacío...). 
+En caso de ser correcto, se mostrará el borde
+del input en verde. En caso de ser incorrecto, se mostrará el
+borde del input en rojo y se añadirá debajo un texto en rojo
+indicando que no se cumplen los requisitos.
+*/
+
+
